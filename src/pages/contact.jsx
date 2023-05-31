e9d1f89e50fce7494aded81d032b842bdbf356b9
cheungty1 <99298338+cheungty1@users.noreply.github.com>
2023-05-31 18:10:53 +1000

    contact page added head

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Fragment } from 'react';
+import Head from 'next/head';
 
 const Contact = () => {
   return (
+    <Fragment>
+      <Head>
+        <title>iWorld - Contact Us</title>
+        <meta 
+          name='description'
+          description='Contact Us'
+        />
+      </Head>
+
     <Container className="my-4 text-left">
       <h2 className="mb-4">Contact Us</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore debitis omnis quod porro doloremque maxime enim ipsa quae, quidem voluptatem. Nostrum animi exercitationem corrupti, nesciunt tempore illum possimus nihil cupiditate eveniet velit repellendus accusantium, molestias veniam consectetur nisi ipsam assumenda suscipit architecto, eos obcaecati eaque iure excepturi. Sed quod nesciunt minima quis dolore? Iure delectus dignissimos beatae, eaque enim quam a labore porro cumque minima ratione ducimus explicabo ut eum hic eveniet! Unde sit voluptatem assumenda veniam officia illum fuga laboriosam ab! Reprehenderit eligendi modi dolor, illo est aperiam nobis quasi illum eius possimus, recusandae odit deserunt dignissimos? Aliquid, eveniet?</p>
@@ -33,6 +44,7 @@ const Contact = () => {
       </Button>
     </Form>
     </Container>
+    </Fragment>
   )
 }
 
