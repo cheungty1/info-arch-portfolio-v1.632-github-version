d9e7d3841ac4bf40e5fedcfaf096d60ec9ae748e
cheungty1 <99298338+cheungty1@users.noreply.github.com>
2023-05-31 17:56:18 +1000

    deleted head

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Fragment } from 'react';
-import Head from 'next/head';
 
 const Contact = () => {
   return (
